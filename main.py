e5aac0d0cb49f73d294df1c5b1e9b7f1e16e41b9
Arturo Crespo de la Viña <acrespo@MacBook-Pro-de-Arturo.local>
2018-06-06 21:54:49 +0100

    Completed TODO sections

--- main.py
+++ main.py
@@ -33,7 +33,15 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    
+    return (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -61,7 +69,12 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    # logits and labels must have the same shape, e.g. [batch_size, num_classes] and the same dtype (either float16, float32, or float64).
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+    train_op = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cross_entropy_loss)
+    
+    return (logits, train_op, cross_entropy_loss)
 tests.test_optimize(optimize)
 
 
@@ -81,7 +94,18 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
+    print("Starting Training")
+    
+    for epoch in range(epochs):
+        for image, label in get_batches_fn(batch_size):
+            
+            _, loss = sess.run(
+                       [train_op,cross_entropy_loss],
+                       feed_dict={
+                           input_image: image,
+                           correct_label: label,
+                           keep_prob: 0.8})
+        print("  Epoch {} - Loss = {:.3f}".format(epoch + 1, loss))
 tests.test_train_nn(train_nn)
 
 
@@ -109,11 +133,27 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        image_input, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out = load_vgg(sess, vgg_path)
+        nn_last_layer = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes=2)
+        logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, 2)
 
         # TODO: Train NN using the train_nn function
+        sess.run(tf.global_variables_initializer())
+        train_nn(
+                 sess=sess,
+                 epochs=20,
+                 batch_size=8,
+                 get_batches_fn=get_batches_fn,
+                 train_op=train_op,
+                 cross_entropy_loss=cross_entropy_loss,
+                 input_image=image_input,
+                 correct_label=correct_label,
+                 keep_prob=keep_prob,
+                 learning_rate=learning_rate)
 
         # TODO: Save inference data using helper.save_inference_samples
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, image_input)
 
         # OPTIONAL: Apply the trained model to a video
 
